c74e8dd5f2d2c21bbfe0601352c2e5f1f3d717fb
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2026-04-10 22:28:49 -0400

    Fix standalone pcbnew footprint preload ordering
    Fixes: https://gitlab.com/kicad/code/kicad/-/work_items/23872

--- common/single_top.cpp
+++ common/single_top.cpp
@@ -420,12 +420,6 @@ bool PGM_SINGLE_TOP::OnPgmInit()
     // Load library tables after startup wizard
     GetLibraryManager().LoadGlobalTables();
 
-    // Preload libraries, since for single-top this won't have been done earlier
-    if( KIFACE* topFrame = Kiway.KiFACE( KIWAY::KifaceType( TOP_FRAME ) ) )
-        topFrame->PreloadLibraries( &Kiway );
-
-    PreloadDesignBlockLibraries( &Kiway );
-
     App().SetTopWindow( frame );      // wxApp gets a face.
     App().SetAppDisplayName( frame->GetAboutTitle() );
 
@@ -489,6 +483,13 @@ bool PGM_SINGLE_TOP::OnPgmInit()
 
         frame->OpenProjectFiles( fileArgs );
     }
+
+    // In single-top mode, OpenProjectFiles() may switch to a different project. Preload
+    // libraries only after that so project-local libraries are loaded for the active project.
+    if( KIFACE* topFrame = Kiway.KiFACE( KIWAY::KifaceType( TOP_FRAME ) ) )
+        topFrame->PreloadLibraries( &Kiway );
+
+    PreloadDesignBlockLibraries( &Kiway );
 
 #ifdef KICAD_IPC_API
     m_api_server->SetReadyToReply();
